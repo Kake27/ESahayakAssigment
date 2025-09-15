8bc5ffca431e08c1cac7f415ff1a4b94e8963adc
Kake27 <karekarayoosh@gmail.com>
2025-09-15 14:28:29 +0530

    deployment fixes

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "src/generated/**", 
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", 
+    },
+  },
+];
